d191b0805316419b5c7eae50d10217a1bde81290
Lindsay Chapin <lnchapin@gmail.com>
2017-10-25 11:09:04 -0600

    on a server

--- src/github.com/lnchapin/hello/hello.go
+++ src/github.com/lnchapin/hello/hello.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/lnchapin/string"
 )
 
+type MyHandler int
+
+func (h MyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/reverse":
+		io.WriteString(res, string.Reverse("Hello, new gopher!"))
+	case "/":
+		io.WriteString(res, "Hello, new gopher!")
+	}
+}
+
 func main() {
-	fmt.Println("Hello, new gopher!")
-	fmt.Println(string.Reverse("Hello, new gopher!"))
+	var h MyHandler
+	http.ListenAndServe(":9000", h)
 }
